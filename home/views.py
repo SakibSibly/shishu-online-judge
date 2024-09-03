590a252dd44a5608b381e992ed6406f8c922dad4
SakibSibly <sibly@duck.com>
2024-09-03 17:44:46 +0600

    Css file and Context for the home added

--- home/views.py
+++ home/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, HttpResponse
 from django.views import View
-import os, subprocess
+from .models import Post
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/home.html')
+        latest_posts = Post.objects.all().order_by('-created_at')[:5]
+        context = {
+            'latest_posts': latest_posts
+        }
+        return render(request, 'home/home.html', context)
     
     def post(self, request):
         pass
