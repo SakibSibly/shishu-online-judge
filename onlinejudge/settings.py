c109c2c5e0be226fdd692dea52b9e3f07d841d31
SakibSibly <sibly@duck.com>
2024-09-03 16:36:25 +0600

    Global files added and Allowed host made open

--- onlinejudge/settings.py
+++ onlinejudge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(81qihuavquf#x#my43w+^eguuj3f2-@s285sso@jl)755pd#t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    'globals.apps.GlobalsConfig',
 ]
 
 MIDDLEWARE = [
