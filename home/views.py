79536cbbdfca63c4dc03c6815e9aa7a868c49553
SakibSibly <sibly@duck.com>
2024-09-03 14:53:08 +0600

    python feature added and c++ execution modified

--- home/views.py
+++ home/views.py
@@ -10,29 +10,47 @@ class HomeView(View):
     def post(self, request):
         obj = request.POST
         main_code = obj['code']
-        return self.execute_cpp_code(main_code)
+        language = obj['language']
 
+        if language == "2":
+            return self.execute_python_code(main_code)
+        elif language == "1":
+            return self.execute_cpp_code(main_code)
 
     def execute_cpp_code(self, cpp_code):
         cpp_file = "main.cpp"
         with open(cpp_file, "w") as file:
             file.write(cpp_code)
         
-        compile_command = ["g++", cpp_file, "-o", "temp_program"]
+        compile_command = ["g++", cpp_file, "-o", "main"]
         compilation = subprocess.run(compile_command, capture_output=True, text=True)
         
         if compilation.returncode != 0:
             return HttpResponse("Compilation failed:\n" + compilation.stderr)
         
-        execution_command = ["./temp_program"]
+        execution_command = ["./main"]
         execution = subprocess.run(execution_command, capture_output=True, text=True)
         
-
-        try:
-            if execution.returncode == 0:
-                return HttpResponse("Program Output:\n" + execution.stdout)
-            else:
-                return HttpResponse("Program failed to execute:\n" +execution.stderr)
-        finally:
+        if execution.returncode == 0:
             os.remove(cpp_file)
-            os.remove("temp_program")
+            os.remove("main")
+            return HttpResponse("Program Output:\n" + execution.stdout)
+        else:
+            os.remove(cpp_file)
+            os.remove("main")
+            return HttpResponse("Program failed to execute:\n" +execution.stderr)
+
+    def execute_python_code(self, python_code):
+        python_file = "main.py"
+        with open(python_file, "w") as file:
+            file.write(python_code)
+        
+        execution_command = ["python", python_file]
+        execution = subprocess.run(execution_command, capture_output=True, text=True)
+
+        if execution.returncode == 0:
+            os.remove(python_file)
+            return HttpResponse("Program Output:\n" + execution.stdout)
+        else:
+            os.remove(python_file)
+            return HttpResponse("Program failed to execute:\n" +execution.stderr)
