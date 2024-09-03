1379633d09cbff5e562f34960cfd31b4ea594abf
SakibSibly <sibly@duck.com>
2024-09-04 01:01:53 +0600

    I/O bug fixed for files that needs input from the user

--- customtest/views.py
+++ customtest/views.py
@@ -18,17 +18,18 @@ class CustomTest(View):
             obj = request.POST
             main_code = obj['code']
             language = obj['language']
+            input_data = obj['input_data']
 
             if language == "0":
-                return self.execute_c_code(request, main_code)
+                return self.execute_c_code(request, main_code, input_data)
             elif language == "1":
-                return self.execute_cpp_code(request, main_code)
+                return self.execute_cpp_code(request, main_code, input_data)
             elif language == "2":
-                return self.execute_python_code(request, main_code)
+                return self.execute_python_code(request, main_code, input_data)
         else:
             return HttpResponse("Invalid Character found in the source code")
         
-    def execute_c_code(self, request, cpp_code):
+    def execute_c_code(self, request, cpp_code, input_data):
         c_file = "main.c"
         with open(c_file, "w") as file:
             file.write(cpp_code)
@@ -44,24 +45,24 @@ class CustomTest(View):
             return render(request, 'customtest/output.html', context)
         
         execution_command = ["./main"]
-        execution = subprocess.run(execution_command, capture_output=True, text=True)
+        execution = subprocess.run(execution_command, input=input_data, capture_output=True, text=True)
         
         if execution.returncode == 0:
             os.remove(c_file)
             os.remove("main")
             context = {
-                'output': "Program Output:\n" + execution.stdout
+                'output': execution.stdout
             }
             return render(request, 'customtest/output.html', context)
         else:
             os.remove(c_file)
             os.remove("main")
             context = {
-                'output': "Program failed to execute:\n" +execution.stderr
+                'output': "Program failed to execute:\n" + execution.stderr
             }
             return render(request, 'customtest/output.html', context)
         
-    def execute_cpp_code(self, request, cpp_code):
+    def execute_cpp_code(self, request, cpp_code, input_data):
         cpp_file = "main.cpp"
         with open(cpp_file, "w") as file:
             file.write(cpp_code)
@@ -77,30 +78,30 @@ class CustomTest(View):
             return render(request, 'customtest/output.html', context)
         
         execution_command = ["./main"]
-        execution = subprocess.run(execution_command, capture_output=True, text=True)
+        execution = subprocess.run(execution_command, input=input_data, capture_output=True, text=True)
         
         if execution.returncode == 0:
             os.remove(cpp_file)
             os.remove("main")
             context = {
-                'output': "Program Output:\n" + execution.stdout
+                'output': execution.stdout
             }
             return render(request, 'customtest/output.html', context)
         else:
             os.remove(cpp_file)
             os.remove("main")
             context = {
-                'output': "Program failed to execute:\n" +execution.stderr
+                'output': "Program failed to execute:\n" + execution.stderr
             }
             return render(request, 'customtest/output.html', context)
 
-    def execute_python_code(self, request, python_code):
+    def execute_python_code(self, request, python_code, input_data):
         python_file = "main.py"
         with open(python_file, "w") as file:
             file.write(python_code)
         
         execution_command = ["python", python_file]
-        execution = subprocess.run(execution_command, capture_output=True, text=True)
+        execution = subprocess.run(execution_command, input=input_data, capture_output=True, text=True)
 
         if execution.returncode == 0:
             os.remove(python_file)
