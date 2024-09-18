c6545d73f0f1aea468c75cb7892c16e16356194f
SakibSibly <sibly@duck.com>
2024-09-19 00:29:20 +0600

    Login is imposed to submit code to the judge

--- customtest/views.py
+++ customtest/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.urls import reverse
+from django.utils.http import urlencode
 from django.views import View
 from .forms import CodeSubmissionForm
 from globals.executor import CodeExecutor
@@ -13,35 +15,38 @@ class CustomTest(View):
         return render(request, 'customtest/custom.html', context)
     
     def post(self, request):
-        form = CodeSubmissionForm(request.POST)
-        if form.is_valid():
-            obj = request.POST
-            source_code = obj['code']
-            language = obj['language']
-            input_data = obj['input_data']
+        if request.user.is_authenticated:
+            form = CodeSubmissionForm(request.POST)
+            if form.is_valid():
+                obj = request.POST
+                source_code = obj['code']
+                language = obj['language']
+                input_data = obj['input_data']
 
-            executor = CodeExecutor()
-            context = {}
+                executor = CodeExecutor()
+                context = {}
 
-            if language == "0":
-                output = executor.execute_c_code(source_code, input_data, 1)
-                context = {
-                    'output': output[0],
-                    'time': "Execution time: " + output[1] + " seconds"
-                }
-            elif language == "1":
-                output = executor.execute_cpp_code(source_code, input_data, 1)
-                context = {
-                    'output': output[0],
-                    'time': "Execution time: " + output[1] + " seconds"
-                }
-            elif language == "2":
-                output = executor.execute_python_code(source_code, input_data, 1)
-                context = {
-                    'output': output[0],
-                    'time': "Execution time: " + output[1] + " seconds"
-                }
-                
-            return render(request, 'customtest/output.html', context)
-        else:
-            return HttpResponse("Invalid Character found in the source code")
+                if language == "0":
+                    output = executor.execute_c_code(source_code, input_data, 1)
+                    context = {
+                        'output': output[0],
+                        'time': "Execution time: " + output[1] + " seconds"
+                    }
+                elif language == "1":
+                    output = executor.execute_cpp_code(source_code, input_data, 1)
+                    context = {
+                        'output': output[0],
+                        'time': "Execution time: " + output[1] + " seconds"
+                    }
+                elif language == "2":
+                    output = executor.execute_python_code(source_code, input_data, 1)
+                    context = {
+                        'output': output[0],
+                        'time': "Execution time: " + output[1] + " seconds"
+                    }
+                    
+                return render(request, 'customtest/output.html', context)
+            else:
+                return HttpResponse("Invalid Character found in the source code")
+        login_url = reverse('login') + '?' + urlencode({'next': request.path})
+        return redirect(login_url)
--- problems/views.py
+++ problems/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.utils.http import urlencode
 from django.views import View
 from .models import Problem, InputOutput
 from customtest.forms import CodeSubmissionForm
@@ -26,37 +28,40 @@ class FetchProblemView(View):
         return render(request, 'problems/individual_problem.html', context)
     
     def post(self, request, id):
-        source_code = request.POST['code']
-        language = request.POST['language']
-        ios = InputOutput.objects.filter(problem=Problem.objects.filter(id=id)[0])
-        test_cases = []
-        for element in ios:
-            test_cases.append([element.input, element.output, element.problem.time_limit])
+        if request.user.is_authenticated:       
+            source_code = request.POST['code']
+            language = request.POST['language']
+            ios = InputOutput.objects.filter(problem=Problem.objects.filter(id=id)[0])
+            test_cases = []
+            for element in ios:
+                test_cases.append([element.input, element.output, element.problem.time_limit])
+            
+
+            for case in test_cases:
+                executor = CodeExecutor()
+                if language == "0":
+                    output = executor.execute_c_code(source_code, case[0], case[2])
+                elif language == "1":
+                    output = executor.execute_cpp_code(source_code, case[0], case[2])
+                elif language == "2":
+                    output = executor.execute_python_code(source_code, case[0], case[2])
+
+                if executor.compare_outputs(output[0].strip(), case[1].strip()) == False:
+                    context = {
+                        'output': f"Test case failed:\n\nInput:\n{case[0]}\n\nExpected:\n{case[1]} \n\n\nReceived:\n{output[0]}",
+                        'time': "Execution time: " + output[1] + " seconds"
+                    }
+                    with open("test.txt", "w+") as file:
+                        file.write(output[0])
+                    with open("test2.txt", "w+") as file:
+                        file.write(case[1])
+                    
+                    return render(request, 'customtest/output.html', context)
         
-
-        for case in test_cases:
-            executor = CodeExecutor()
-            if language == "0":
-                output = executor.execute_c_code(source_code, case[0], case[2])
-            elif language == "1":
-                output = executor.execute_cpp_code(source_code, case[0], case[2])
-            elif language == "2":
-                output = executor.execute_python_code(source_code, case[0], case[2])
-
-            if executor.compare_outputs(output[0].strip(), case[1].strip()) == False:
-                context = {
-                    'output': f"Test case failed:\n\nInput:\n{case[0]}\n\nExpected:\n{case[1]} \n\n\nReceived:\n{output[0]}",
-                    'time': "Execution time: " + output[1] + " seconds"
-                }
-                with open("test.txt", "w+") as file:
-                    file.write(output[0])
-                with open("test2.txt", "w+") as file:
-                    file.write(case[1])
-                
-                return render(request, 'customtest/output.html', context)
-    
-        context = {
-            'output': "All test cases passed",
-            'time': "Execution time: " + output[1] + " seconds"
-        }
-        return render(request, 'customtest/output.html', context)
+            context = {
+                'output': "All test cases passed",
+                'time': "Execution time: " + output[1] + " seconds"
+            }
+            return render(request, 'customtest/output.html', context)
+        login_url = reverse('login') + '?' + urlencode({'next': request.path})
+        return redirect(login_url)
