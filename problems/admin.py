4b3d2b0cf959e8393f1785e9944aef973d601b9b
SakibSibly <sibly@duck.com>
2024-09-19 16:53:07 +0600

    SolvedData datamodel is registered to admin site

--- problems/admin.py
+++ problems/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Problem, InputOutput
+from .models import Problem, InputOutput, SolvedData
 
 
 admin.site.register(Problem)
 admin.site.register(InputOutput)
+admin.site.register(SolvedData)
