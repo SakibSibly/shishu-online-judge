132d0ed63f9624dc576598681bc7625d8500e370
SakibSibly <sibly@duck.com>
2024-09-23 08:52:28 +0600

    Data Models registered for admin site

--- contests/admin.py
+++ contests/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Contest, ContestProblem
+from .models import Contest, ContestProblem, ContestRegData, ContestSubmission, ContestLeaderboard
 
 
 admin.site.register(Contest)
 admin.site.register(ContestProblem)
+admin.site.register(ContestRegData)
+admin.site.register(ContestSubmission)
+admin.site.register(ContestLeaderboard)
